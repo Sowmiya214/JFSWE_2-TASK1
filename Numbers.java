1d8b431f142626218f3510faaf3e394788351cb5
Sowmiya <114464938+Sowmiya214@users.noreply.github.com>
2024-01-09 21:21:40 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,20 @@
+package com.java.JFSWE_2.Task1;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+
+		System.out.println("print the number 10 to 50 using for loop");
+		for (int i = 10; i <= 50; i++) {
+			System.out.println(i);
+		}
+		// print the number 10 to 50 using while loop
+		System.out.println("print the number 10 to 50 using while loop");
+		int i = 10;
+		while (i <= 50) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
